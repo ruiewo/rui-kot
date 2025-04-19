541ff622c6ae90f5f5a4c79db874b29e6abaff6f
t.wada <109674512+ruiewo@users.noreply.github.com>
2025-04-19 21:25:28 +0900

    delete unused code.

--- src/background.ts
+++
@@ -1,8 +0,0 @@
-console.log("background.ts");
-
-chrome.runtime.onInstalled.addListener(() => {
-	console.log("KingOfTime 勤怠自動入力拡張がインストールされました。");
-});
-
-// タスクの進行状況を管理するためのロジックをここに追加します。
-// 必要に応じてメッセージングを使用してコンテンツスクリプトと通信します。
